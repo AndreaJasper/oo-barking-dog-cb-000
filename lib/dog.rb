133f2039268b53808a41f45c02802cc57b56b806
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-06 21:26:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,18 @@
 # Your code goes here!
 class Dog
-  def initialize(name)
-    @name = name
+  def initialize(dog_name)
+    @name = dog_name
   end
 
   def name
     @name
+  end
+
+  def initialize(bark)
+    @bark = name
+  end
+
+  def bark
+    @bark
   end
 end
